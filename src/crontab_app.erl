8c3e4988280111e3842db91dad2fb6b1fcb4a9fa
Ionut Hristodorescu <ionut@nettologic.com>
2017-06-19 23:30:27 -0700

    start the app with params

--- src/crontab_app.erl
+++ src/crontab_app.erl
@@ -15,10 +15,28 @@
 
 %%%_* Code =============================================================
 start(normal, Args) ->
-  crontab_sup:start_link(Args).
+  case crontab_sup:start_link(Args) of 
+    {of, Pid} -> 
+      setup(), 
+      {ok, Pid};
+    Error -> 
+      Error
+  end.
 
 stop(_S) ->
   ok.
+
+
+setup() ->
+  case application:get_env(crontab, jobs) of 
+    {ok, Crontab} ->
+      lists:foreach(fun([Name, Spec, MFA]) ->
+        crontab:add(Name, Spec, MFA) 
+      end,
+      Crontab);
+    undefined ->
+      ok
+  end.
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
